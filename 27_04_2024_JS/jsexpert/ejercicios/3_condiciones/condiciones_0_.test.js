d07603ccf113ba96b7d34932b2b26ae10218c63b
amlndz <alejandromelendez2014@gmail.com>
2024-04-27 12:38:08 +0200

    [27-04-2024] - clase js

--- 27_04_2024_JS/jsexpert/ejercicios/3_condiciones/condiciones_0_.test.js
+++ 27_04_2024_JS/jsexpert/ejercicios/3_condiciones/condiciones_0_.test.js
@@ -1,4 +1,3 @@
-
 const valores = [
   1,
   "1",
@@ -8,24 +7,23 @@ const valores = [
   false,
   "1º",
   true,
-  '1',
+  "1",
   undefined,
   () => 1,
-]
+];
 
 let sumaDeUnos = 0;
 
 for (let i = 0; i < valores.length; i++) {
   //--------- ⬇️ Modifica el código de abajo ⬇️ ------------//
 
-  if (valores[i] == 1) {
+  if (valores[i] === 1) {
     sumaDeUnos = sumaDeUnos + 1;
   }
 
   //--------- ⬆️ Modifica el código de arriba ⬆️ -----------//
 }
 
-it('El total de unos debería ser 1', () => {
+it("El total de unos debería ser 1", () => {
   expect(sumaDeUnos).toBe(1);
-})
-
+});
