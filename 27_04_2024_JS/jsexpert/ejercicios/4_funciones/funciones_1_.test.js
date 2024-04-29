bac47e1a2b56d858e965072edeb0c0e18341afd6
amlndz <alejandromelendez2014@gmail.com>
2024-04-29 22:24:38 +0200

    [29-04-2024] - Terminar Tests JS

--- 27_04_2024_JS/jsexpert/ejercicios/4_funciones/funciones_1_.test.js
+++ 27_04_2024_JS/jsexpert/ejercicios/4_funciones/funciones_1_.test.js
@@ -1,10 +1,8 @@
-
 //--------- ⬇️ Modifica el código de abajo ⬇️ ------------//
-let miFuncion;
-
+let miFuncion = (a, b) => a + b;
 
 //--------- ⬆️ Modifica el código de arriba ⬆️ -----------//
 
 it("miFuncion debería devolver la suma de dos valores", () => {
   expect(miFuncion(1, 2)).toBe(3);
-} );
+});
