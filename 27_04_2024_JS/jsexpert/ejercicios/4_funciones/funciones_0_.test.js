c1ada62045a690b8395caf15a7fe500eb1fdfaf7
amlndz <alejandromelendez2014@gmail.com>
2024-04-30 17:31:46 +0200

    [0] errors

--- 27_04_2024_JS/jsexpert/ejercicios/4_funciones/funciones_0_.test.js
+++ 27_04_2024_JS/jsexpert/ejercicios/4_funciones/funciones_0_.test.js
@@ -1,10 +1,9 @@
-
 //--------- ⬇️ Modifica el código de abajo ⬇️ ------------//
-let miFuncion;
-
+let miFuncion = function () {};
 
 //--------- ⬆️ Modifica el código de arriba ⬆️ -----------//
-
-it("miFuncion debería ser una función", () => {
-  expect(typeof miFuncion).toBe("function");
-} );
+describe("Sabiendo que solo puedes usar la letra a y la b:\n", () => {
+  it("miFuncion debería ser una función", () => {
+    expect(typeof miFuncion).toBe("function");
+  });
+});
