39c2f16acfe93a16fb22c1686bfdedf1475d7933
amlndz <alejandromelendez2014@gmail.com>
2024-05-14 18:29:09 +0200

    implementacion test es Buzz

--- 05_14_2024_testing/testing-playground/src/fizbuzz.test.ts
+++ 05_14_2024_testing/testing-playground/src/fizbuzz.test.ts
@@ -7,4 +7,7 @@ describe(`Si el numero es múltipo de 3. debe devolver "Fizz"`, () => {
   it("Dado una cadena de numeros con un numero multiplo de 3 devuelve la cadena sustituyendo el numero por Fizz", async () => {
     expect(fizzBuzz([1, 2, 3])).toStrictEqual(["1", "2", "Fizz"]);
   });
+  it("Dado una cadena de numeros con un numero multiplo de 5 devuelve la cadena sustituyendo el numero por Buzz", async () => {
+    expect(fizzBuzz([1, 2, 5])).toStrictEqual(["1", "2", "Buzz"]);
+  });
 });
--- 05_14_2024_testing/testing-playground/src/fizzBuzz.ts
+++ 05_14_2024_testing/testing-playground/src/fizzBuzz.ts
@@ -3,6 +3,9 @@ export function fizzBuzz(numbers: number[]): string[] {
     if (number % 3 === 0) {
       return "Fizz";
     }
+    if (number % 5 === 0) {
+      return "Buzz";
+    }
     return number.toString();
   });
 
