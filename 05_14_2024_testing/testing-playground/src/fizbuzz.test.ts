37af0a17c593ea9bb04138214a1fc18286a6f18e
amlndz <alejandromelendez2014@gmail.com>
2024-05-14 18:34:02 +0200

    Implementacion test es FizzBuzz

--- 05_14_2024_testing/testing-playground/src/fizbuzz.test.ts
+++ 05_14_2024_testing/testing-playground/src/fizbuzz.test.ts
@@ -1,13 +1,19 @@
 import { fizzBuzz } from "./fizzBuzz.ts";
 
 describe(`Si el numero es múltipo de 3. debe devolver "Fizz"`, () => {
-  it("Dado una cadena de numeros sin numeros multiplos de 3 ni de 5 recibe la cadena tal cual", async () => {
+  it("Dado una cadena de numeros sin numeros multiplos de 3 ni de 5 recibe la cadena tal cual", () => {
     expect(fizzBuzz([1, 2, 4])).toStrictEqual(["1", "2", "4"]);
   });
-  it("Dado una cadena de numeros con un numero multiplo de 3 devuelve la cadena sustituyendo el numero por Fizz", async () => {
+  it("Dado una cadena de numeros con un numero multiplo de 3 devuelve la cadena sustituyendo el numero por Fizz", () => {
     expect(fizzBuzz([1, 2, 3])).toStrictEqual(["1", "2", "Fizz"]);
   });
-  it("Dado una cadena de numeros con un numero multiplo de 5 devuelve la cadena sustituyendo el numero por Buzz", async () => {
+  it("Dado una cadena de numeros con un numero multiplo de 5 devuelve la cadena sustituyendo el numero por Buzz", () => {
     expect(fizzBuzz([1, 2, 5])).toStrictEqual(["1", "2", "Buzz"]);
+  });
+  it("Dado una cadena vacia devuelve una cadena vacia", async () => {
+    expect(fizzBuzz([])).toStrictEqual([]);
+  });
+  it("Dado una cadena de numeros que contiene un numero multiplo de 3 y de 5 lo sustituye por FizzBuzz", () => {
+    expect(fizzBuzz([13, 26, 30])).toStrictEqual(["13", "26", "FizzBuzz"]);
   });
 });
--- 05_14_2024_testing/testing-playground/src/fizzBuzz.ts
+++ 05_14_2024_testing/testing-playground/src/fizzBuzz.ts
@@ -1,5 +1,8 @@
 export function fizzBuzz(numbers: number[]): string[] {
   const numbersFizzBuzz: string[] = numbers.map((number) => {
+    if (number % 3 === 0 && number % 5 === 0) {
+      return "FizzBuzz";
+    }
     if (number % 3 === 0) {
       return "Fizz";
     }
