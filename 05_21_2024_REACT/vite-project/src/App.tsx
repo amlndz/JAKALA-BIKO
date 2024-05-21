5b4517dcb19bff6bfa4f47c90e03e094fa73fdc5
amlndz <alejandromelendez2014@gmail.com>
2024-05-21 18:59:36 +0200

    ejercicio 1.2

--- 05_21_2024_REACT/vite-project/src/App.tsx
+++ 05_21_2024_REACT/vite-project/src/App.tsx
@@ -1,11 +1,46 @@
 import "./App.css";
 
-function App() {
+// type Props = {
+//   num: number;
+// };
+
+// export const MySecondComponent: React.FC<Props> = ({ num }) => {
+//   return createElement(
+//     "li",
+//     { className: "my-list-element" },
+//     `elemento ${num}`
+//   );
+// };
+
+// export const MyFirstComponent = () => {
+//   return createElement(
+//     "ul",
+//     { id: "my-list" },
+//     <MySecondComponent num={1} />,
+//     <MySecondComponent num={2} />,
+//     <MySecondComponent num={3} />
+//   );
+// };
+
+const ItemComponent = () => {
+  return <li className="item-class">Elemento Lista</li>;
+};
+
+const MyFirstComponent = () => {
+  return (
+    <ul className="my-list-class">
+      <ItemComponent />
+      <ItemComponent />
+      <ItemComponent />
+    </ul>
+  );
+};
+
+export const App = () => {
   return (
     <>
       <h1>MI LISTA</h1>
+      <MyFirstComponent />
     </>
   );
-}
-
-export default App;
+};
